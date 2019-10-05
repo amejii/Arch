7fddb9a3a0d55232d1637149c2c135c9349de904
amejii <nanahachiroku786@gmail.com>
2019-10-05 22:06:10 +0900

    Add singleton

---
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour 
+    where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance{
+        get{
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                if(_instance == null)
+                {
+                    Debug.Log(string.Format("Can not find {0} !",typeof(T)));
+                }
+            }
+            return _instance;
+        }
+    }
+}
